4ecc5ef75c6115e50087162ab6a8e47c03e7cdff
hemraj_lovewanshi_ <hemrajlodha0007@gmail.com>
2026-05-19 10:20:57 +0000

    data types

--- a1/a2.js
+++ a1/a2.js
@@ -1,2 +1,27 @@
 "use strict"
 console.log("hemraj")
+// alert(3+3)//we are using nodejs,not browser
+console.log(3-3098)
+
+//code readibility should be high
+let name="hemraj"
+let  age =19
+let isloggin =false
+let state
+
+
+
+// number >=2 to power 53
+//bigint 
+//string=>""
+//boolean >= true /false
+//null=> standlone value
+//undefined=>
+//symbol=>unque
+
+//object
+console.log(typeof "hemraj")
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined)
+
