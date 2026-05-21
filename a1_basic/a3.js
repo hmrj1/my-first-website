30a89dd46511453b9f8b1ed95fd86b0f5515f03f
hemraj_lovewanshi_ <hemrajlodha0007@gmail.com>
2026-05-21 08:40:08 +0000

    basic

---
+++ a1_basic/a3.js
@@ -0,0 +1,40 @@
+let setnumber =34
+//let stringnumber =String(somenumber)
+//console.log(stringnumber);
+//console.log(typeof stringnumber);
+
+             /* Operations*/ 
+
+let value=45
+let negvalue=-value
+console.log(negvalue);
+console.log(2+5);
+console.log(2-5);
+console.log(2*5);
+console.log(2**5);
+console.log(2/5);
+console.log(2%5);
+
+let str1="hello"
+let str2=" world"
+let ste4="world"
+//str5=str1+" "+str4
+//not allowed 
+let str3= str1+str2
+console.log( str3)
+//console.log( str5)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+//if first is string then treat as all string 
+console.log(1+2+"2"+2+1)
+//when string in last till string normal from string behave string 
+console.log(true)
+console.log(+true)
+console.log(+"")
+let num1,num2,num3
+num1=num2=num3=4
+let gamecounter=100
+gamecounter++
+console.log(++gamecounter)
+console.log(gamecounter++)
---
+++ a1_basic/a4.js
@@ -0,0 +1,15 @@
+console.log(2>1)
+console.log(2<1)
+console.log(2!=1)
+console.log(2==1)
+console.log("2">1)
+//yaha pata nii kese ye is string ko vi int man raha hai but hame type consrevation ka dhyan rakhna hai
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+//ye sab operations galat hai bas jo uper ke 4 operations hai wo sare hi ye tarika sahi nahi hai  
+
+//   ===
+console.log("2"===2)
+
+
